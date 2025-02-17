44d74f4dac05694afb0a888075ce94ec5f449e56
Adejare <adejare.arasheed@gmail.com>
2025-02-17 12:08:48 +0100

    Three Sum

--- medium/3sum.py
+++ medium/3sum.py
@@ -2,25 +2,66 @@
 from typing import List
 
 
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         # Use three pointers. one static, two dynamic
+#         sorted_list = sorted(nums)
+#         last_list = []
+#         result = []
+
+#         for i in range(len(sorted_list) - 2):
+#             left = i+1
+#             right = len(sorted_list) - 1
+
+#             if sorted_list[i] > 0:
+#                 break
+
+#             if i > 0 and sorted_list[i] == sorted_list[i-1]:
+#                 continue
+
+#             while left < right:
+#                 current_list = [sorted_list[i], sorted_list[left], sorted_list[right]]
+#                 total_sum = sum(current_list)
+
+#                 if (left > i + 1) and sorted_list[left] == sorted_list[left-1]:
+#                     left += 1
+#                     continue
+
+#                 if sum([sorted_list[i], sorted_list[left]]) > 0:
+#                     break
+
+#                 elif total_sum == 0 and current_list != last_list:
+#                     last_list = current_list
+#                     result.append(current_list)
+#                     left += 1
+#                     right -= 1
+
+#                 elif total_sum > 0:
+#                     right -= 1
+
+#                 else:
+#                     left += 1
+
+#         return result
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         # Use three pointers. one static, two dynamic
-        sorted_list = sorted(nums)
         last_list = []
         result = []
 
-        for i in range(len(sorted_list) - 2):
+        for i in range(len(nums) - 2):
             left = i+1
-            right = len(sorted_list) - 1
+            right = len(nums) - 1
 
-            if sorted_list[i] > 0:
+            if nums[i] > 0:
                 break
 
-            if i > 0 and sorted_list[i] == sorted_list[i-1]:
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
 
             while left < right:
-                current_list = [sorted_list[i], sorted_list[left], sorted_list[right]]
+                current_list = [nums[i], nums[left], sorted_list[right]]
                 total_sum = sum(current_list)
 
                 if (left > i + 1) and sorted_list[left] == sorted_list[left-1]:
